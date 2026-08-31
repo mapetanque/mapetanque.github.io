cfcaf984375c4584a10da6323ae078b9f3723e21
mapetanque <mapetanque@outlook.be>
2026-08-31 20:42:10 +0200

    nouveau correctif update terrain

--- scripts/update_terrains.py
+++ scripts/update_terrains.py
@@ -18,8 +18,8 @@ QUERY = """
 area["ISO3166-1"="BE"]["admin_level"="2"]->.belgique;
 
 (
-  nwr["leisure"="pitch"]["sport"="boules"]["access"!~"^(private|customers|no)$"](area.belgique);
-  nwr["leisure"="pitch"]["sport"="petanque"]["access"!~"^(private|customers|no)$"](area.belgique);
+  nwr["leisure"="pitch"]["sport"="boules"]["access"!="private"]["access"!="customers"]["access"!="no"](area.belgique);
+  nwr["leisure"="pitch"]["sport"="petanque"]["access"!="private"]["access"!="customers"]["access"!="no"](area.belgique);
 );
 
 out center tags;
